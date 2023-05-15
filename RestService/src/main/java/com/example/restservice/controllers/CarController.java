b10542103c57b00045b2937085b28be525f4fc3d
Maciej Makara <maciejmakarakontakt@gmail.com>
2023-05-15 17:36:41 +0200

    list5a

--- RestService/src/main/java/com/example/restservice/controllers/CarController.java
+++ RestService/src/main/java/com/example/restservice/controllers/CarController.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class CarController {
 
